bb76521b4790d83f14fecd31efca7a674721a1f6
Roy <roy.baron@sweetgreen.com>
2023-09-17 22:27:29 -0400

    Adds physics on grenades

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -7,13 +7,16 @@ func _on_gate_player_entered_gate(body):
 	print('player has entered gate', body)
 
 
-func _on_player_laser(pos: Vector2):
-	var laser = laser_scene.instantiate()
+func _on_player_laser(pos: Vector2, direction: Vector2):
+	var laser = laser_scene.instantiate() as Area2D
 	laser.position = pos
+	laser.rotation_degrees = rad_to_deg(direction.angle()) + 90
+	laser.direction = direction
 	$Projectiles.add_child(laser)
 
 
-func _on_player_grenade(pos: Vector2):
-	var grenade = grenade_scene.instantiate()
+func _on_player_grenade(pos: Vector2, direction: Vector2):
+	var grenade = grenade_scene.instantiate() as RigidBody2D
 	grenade.position = pos
+	grenade.linear_velocity = direction * grenade.speed
 	$Projectiles.add_child(grenade)
--- scenes/objects/player.gd
+++ scenes/objects/player.gd
@@ -4,31 +4,34 @@ var move_speed: int = 200
 var can_laser: bool = true
 var can_grenade: bool = true
 
-signal laser(position: Vector2)
-signal grenade(position: Vector2)
+signal laser(position: Vector2, direction: Vector2)
+signal grenade(position: Vector2, direction: Vector2)
 
 func _process(_delta):
 	handle_input()
 	
 func handle_input():
 	var direction = Input.get_vector('left', 'right', 'up', 'down')
+	var player_direction = (get_global_mouse_position() - position).normalized()
 	velocity = direction * move_speed
 	move_and_slide()
+	
+	look_at(get_global_mouse_position())
 	
 	if (Input.is_action_pressed('primary action')):
 		if (!can_laser): return
 		var laser_markers = $LaserStartPositions.get_children()
 		# var selected_laser = laser_markers[randi() % laser_markers.size()]
 		var selected_laser = laser_markers.pick_random()
+
 		can_laser = false
 		$LaserTimer.start()
-		
-		laser.emit(selected_laser.global_position)
+		laser.emit(selected_laser.global_position, player_direction)
 		
 	if (Input.is_action_pressed("secondary action")):
 		if (!can_grenade): return
 		
-		grenade.emit($GrenadeStartPosition.global_position)
+		grenade.emit($GrenadeStartPosition.global_position, player_direction)
 		can_grenade = false
 		$GrenadeTimer.start()
 
