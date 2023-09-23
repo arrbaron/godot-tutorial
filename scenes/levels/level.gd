f401541201946d15df429c4764f4156d43f3b983
Roy <roy.baron@sweetgreen.com>
2023-09-23 18:22:06 -0400

    Adds tweens

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -3,8 +3,9 @@ extends Node2D
 var laser_scene: PackedScene = preload("res://scenes/projectiles/laser.tscn")
 var grenade_scene: PackedScene = preload("res://scenes/projectiles/grenade.tscn")
 
-func _on_gate_player_entered_gate(body):
-	print('player has entered gate', body)
+func _on_gate_player_entered_gate(_body):
+	var tween = create_tween()
+	tween.tween_property($Player, 'speed', 0, 0.5)
 
 
 func _on_player_laser(pos: Vector2, direction: Vector2):
@@ -20,3 +21,15 @@ func _on_player_grenade(pos: Vector2, direction: Vector2):
 	grenade.position = pos
 	grenade.linear_velocity = direction * grenade.speed
 	$Projectiles.add_child(grenade)
+
+
+func _on_house_player_entered():
+	var tween = create_tween()
+	tween.set_parallel(true)
+	tween.tween_property($Player/Camera2D2, 'zoom', Vector2(1, 1), 1).from(0.5)
+#	tween.tween_property($Player, 'modulate:a', 0, 2).set_trans(Tween.TRANS_QUAD)
+
+
+func _on_house_player_exited():
+	var tween = create_tween()
+	tween.tween_property($Player/Camera2D2, 'zoom', Vector2(0.6, 0.6), 1)
