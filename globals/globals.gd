64c672ebeb582ea0f3f37aebece6f63c4015098e
Roy <roy.baron@sweetgreen.com>
2023-09-25 19:05:18 -0400

    Uses get set to trigger logic off global state updates

--- globals/globals.gd
+++ globals/globals.gd
@@ -1,4 +1,26 @@
 extends Node
 
-var laser_amount: int = 5
-var grenade_amount: int = 3
+signal laser_amount_change
+signal grenade_amount_change
+signal health_change
+
+var laser_amount: int = 5:
+	get:
+		return laser_amount
+	set(value):
+		laser_amount = value
+		laser_amount_change.emit()
+		
+var grenade_amount: int = 3:
+	get:
+		return grenade_amount
+	set(value):
+		grenade_amount = value
+		grenade_amount_change.emit()
+
+var health: int = 60:
+	get:
+		return health
+	set(value):
+		health = value
+		health_change.emit()
