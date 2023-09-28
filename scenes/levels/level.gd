a6d4c02a49769fb047f1b5d72d900538df8f0c49
roy <baron.roy@gmail.com>
2023-09-28 19:59:54 -0400

    Adds few lil tweaks here and there

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -22,7 +22,6 @@ func _on_player_laser(pos: Vector2, direction: Vector2):
 	laser.rotation_degrees = rad_to_deg(direction.angle()) + 90
 	laser.direction = direction
 	$Projectiles.add_child(laser)
-	$UI.update_laser_text()
 
 
 func _on_player_grenade(pos: Vector2, direction: Vector2):
@@ -30,21 +29,4 @@ func _on_player_grenade(pos: Vector2, direction: Vector2):
 	grenade.position = pos
 	grenade.linear_velocity = direction * grenade.speed
 	$Projectiles.add_child(grenade)
-	$UI.update_grenade_text()
 
-
-func _on_house_player_entered():
-	var tween = create_tween()
-	tween.set_parallel(true)
-	tween.tween_property($Player/Camera2D2, 'zoom', Vector2(1, 1), 1).from(0.5)
-#	tween.tween_property($Player, 'modulate:a', 0, 2).set_trans(Tween.TRANS_QUAD)
-
-
-func _on_house_player_exited():
-	var tween = create_tween()
-	tween.tween_property($Player/Camera2D2, 'zoom', Vector2(0.6, 0.6), 1)
-
-
-func _on_player_update_stats():
-	$UI.update_laser_text()
-	$UI.update_grenade_text()
